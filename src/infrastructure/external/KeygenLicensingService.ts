a69e466139b4b45b3ef17782c863b89ee5c5c809
Giga Mania <hil.giga@gmail.com>
2025-07-15 18:11:47 +0100

    refactor: remove default value assignments, propagate check up to the passer

--- src/infrastructure/external/KeygenLicensingService.ts
+++ src/infrastructure/external/KeygenLicensingService.ts
@@ -5,6 +5,7 @@ import { ILicensingService } from "../../application/ports/ILicensingService";
 
 import { config } from "../../config/config";
 
+
 const {
   KG_PWD,
   KEYGEN_API_URL,
@@ -91,10 +92,10 @@ export class KeygenLicensingService implements ILicensingService {
         data: {
           type: "users",
           attributes: {
-            firstName: data.firstName ? data.firstName : "",
-            lastName: data.lastName ? data.lastName : "",
-            email: data.email ? data.email : "",
-            password: data.password ? data.password : ""
+            firstName: data.firstName,
+            lastName: data.lastName,
+            email: data.email,
+            password: data.password
           }
         }
       };
